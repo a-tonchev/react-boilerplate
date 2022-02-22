69e071d30991b7b17ccb7176c20335d44efef7a0
Anton Tonchev <at.tonchev@gmail.com>
2022-02-22 18:43:16 +0200

    - Cleanup react boilerplate - migrate to react router 6 - clean up old libraries

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { VitePWA } from 'vite-plugin-pwa';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
@@ -8,50 +8,54 @@ import path, { resolve } from 'path';
 import reactJsx from 'vite-react-jsx';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: [{
-      find: '@', replacement: path.resolve(__dirname, 'src'),
-    }],
-  },
-  plugins: [
-    reactJsx(),
-    reactRefresh(),
-    VitePWA({
-      srcDir: 'src',
-      strategies: 'injectManifest',
-      injectRegister: null,
-      filename: 'service-worker.js',
-    }),
-    eslintPlugin(),
-  ],
-  build: {
-    outDir: 'build',
-    assetsDir: 'static',
-    sourcemap: true,
-    rollupOptions: {
-      plugins: [
-        visualizer({
-          filename: resolve(__dirname, 'analyzed.html'),
-          template: 'treemap', // sunburst|treemap|network
-          sourcemap: true,
-        }),
-      ],
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    resolve: {
+      alias: [{
+        find: '@', replacement: path.resolve(__dirname, 'src'),
+      }],
     },
-  },
-  server: {
-    host: process.env.VITE_DEV_HOST || 'localhost',
-    port: process.env.VITE_DEV_PORT || 3000,
-    open: true,
-    fs: {
+    plugins: [
+      reactJsx(),
+      reactRefresh(),
+      VitePWA({
+        srcDir: 'src',
+        strategies: 'injectManifest',
+        injectRegister: null,
+        filename: 'service-worker.js',
+      }),
+      eslintPlugin(),
+    ],
+    build: {
+      outDir: 'build',
+      assetsDir: 'static',
+      sourcemap: true,
+      rollupOptions: {
+        plugins: [
+          visualizer({
+            filename: resolve(__dirname, 'analyzed.html'),
+            template: 'treemap', // sunburst|treemap|network
+            sourcemap: true,
+          }),
+        ],
+      },
+    },
+    server: {
+      host: process.env.VITE_DEV_HOST || 'localhost',
+      port: process.env.VITE_DEV_PORT || 3000,
+      open: true,
+      fs: {
       // Allow serving files from one level up to the project root
-      allow: [
-        '..',
-      ],
+        allow: [
+          '..',
+        ],
+      },
     },
-  },
-  esbuild: {
-    jsxFactory: 'jsx',
-    jsxInject: 'import { jsx } from \'@emotion/react\'',
-  },
-});
+    esbuild: {
+      jsxFactory: 'jsx',
+      jsxInject: 'import { jsx } from \'@emotion/react\'',
+    },
+  });
+};
