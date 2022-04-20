cbb7f915c2a66b2ed4ae39bec8717ab5bb5b8e02
Juan Medina <juan@epactnetwork.com>
2022-04-20 07:54:13 -0400

    Create route and components to display modal gallery

---
+++ src/screens/test/Showcase.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable max-len */
+import Header from './Header';
+
+const Showcase = () => (
+  <Header
+    imageUrl="https://bit.ly/3BtHOWh"
+    title="Training Title"
+    buttonLink="/"
+    buttonText="Open Category"
+    items={[
+      {
+        price: '$42.50',
+        title: 'Product 1',
+        description: 'Domyos',
+        imageUrls: {
+          frontFace: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FrontFace.png',
+          frontFaceHalf: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FrontFace_Half.png',
+          frontFaceCutout: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FrontFace_Cutout.png',
+          fullView: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FullView.png',
+          internet: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_Internet.png',
+          sideWall: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_Sidewall.png',
+          thumbnail: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/thumbnail.png',
+        },
+      },
+      {
+        price: '$14.90',
+        title: 'Product 1',
+        description: 'Domyos',
+        imageUrls: {
+          frontFace: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FrontFace.png',
+          frontFaceHalf: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FrontFace_Half.png',
+          frontFaceCutout: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FrontFace_Cutout.png',
+          fullView: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FullView.png',
+          internet: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_Internet.png',
+          sideWall: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_Sidewall.png',
+          thumbnail: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/thumbnail.png',
+        },
+      },
+      {
+        price: '$12.40',
+        title: 'Product 1',
+        description: 'Domyos',
+        imageUrls: {
+          frontFace: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FrontFace.png',
+          frontFaceHalf: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FrontFace_Half.png',
+          frontFaceCutout: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FrontFace_Cutout.png',
+          fullView: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FullView.png',
+          internet: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_Internet.png',
+          sideWall: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_Sidewall.png',
+          thumbnail: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/thumbnail.png',
+        },
+      },
+      {
+        price: '$19.40',
+        title: 'Product 1',
+        description: 'Domyos',
+        imageUrls: {
+          frontFace: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FrontFace.png',
+          frontFaceHalf: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FrontFace_Half.png',
+          frontFaceCutout: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FrontFace_Cutout.png',
+          fullView: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_FullView.png',
+          internet: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_Internet.png',
+          sideWall: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/Agilis_Alpin_Sidewall.png',
+          thumbnail: 'https://files-staging.volvotyre.com/tyreImages/Michelin/Michelin/3528701593869/thumbnail.png',
+        },
+      },
+    ]}
+  />
+);
+
+export default Showcase;
