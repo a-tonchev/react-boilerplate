9c2d57cbf8b2750060b30c9e3c3e22cf804cb1ba
info@suther.de <info@suther.de>
2022-04-26 18:43:21 +0200

    - add functionality to choose if Layout is full-width, or with Header-Bar - optimize footer to get Copyright info from Basic-Config - adjust Designprovider by adding new gray-color (50 / 900) and "palette-mode" (mui5 upgrade)

--- src/components/layout/BasicLayout.jsx
+++ src/components/layout/BasicLayout.jsx
@@ -1,32 +1,67 @@
-import { Container } from '@mui/material';
+import { Container, Grid } from '@mui/material';
+import { useLocation } from 'react-router-dom';
 
+import BasicConfig from '@/components/config/BasicConfig';
 import useClasses from '@/components/layout/hooks/useClasses';
 
 import Header from './header/Header';
 import Footer from './footer/Footer';
 
 const styles = theme => ({
-  topBox: {
-    height: 1,
-    marginTop: 74,
-    [theme.breakpoints.down('sm')]: {
-      marginTop: 66,
+  wrapper: {
+    display: 'flex',
+    backgroundColor: theme.palette.body.background,
+    minHeight: '100vh',
+  },
+  container: {
+    display: 'grid',
+    gridTemplateRows: '1fr',
+  },
+  gridContainer: {
+    marginTop: 84,
+    [theme.breakpoints.down('xs')]: {
+      marginTop: 56,
     },
   },
 });
 
 const BasicLayout = ({ children }) => {
+  const location = useLocation();
   const classes = useClasses(styles);
-  return (
-    <>
-      <Header />
-      <div className={classes.topBox} />
-      <Container maxWidth="lg">
-        {children}
-      </Container>
-      <Footer />
-    </>
+
+  function checkRegExp(arr) {
+    return arr.some(regexp => regexp.test(location.pathname));
+  }
+
+  const noLayout = checkRegExp(
+    [/^\/login/, /^\/signUp/, /^\/passwordRecovery/, /^\/logout/],
   );
+  const fullSizeContent = checkRegExp([/^\/previewSpeedPage/]);
+
+  return noLayout ? (children)
+    : (
+      <div className={classes.wrapper}>
+        <Header />
+        <Grid
+          container
+          direction="row"
+          className={classes.gridContainer}
+        >
+          <Container
+            maxWidth={fullSizeContent ? null : 'lg'}
+            className={classes.container}
+          >
+            {children}
+            <Footer
+              href={BasicConfig.copyright.url}
+              className={{ fontSize: 'smaller' }}
+            >
+              {BasicConfig.copyright.text}
+            </Footer>
+          </Container>
+        </Grid>
+      </div>
+    );
 };
 
 export default BasicLayout;
