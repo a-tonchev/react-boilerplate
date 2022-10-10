de9d375dce8475321ff69e675444418bc182e736
Samuel Reed <samuelreed01@gmail.com>
2022-10-10 20:32:45 +0700

    add CustomForm component

---
+++ src/components/forms/CustomForm.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+import CustomButton from '../inputs/CustomButton';
+import CustomCheckBox from '../inputs/CustomCheckBox';
+import CustomTextField from '../inputs/CustomTextField';
+
+export default function CustomForm({ elements, onSubmit }) {
+  const [formValues, setFormValues] = useState(
+    elements.map(element => {
+      const valueToReturn = element.defaultValue || element.type === 'checkbox' ? false : '';
+      return { label: element.label, value: valueToReturn };
+    }),
+  );
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onSubmit(formValues);
+  }
+
+  function handleChange(newValue, newValueIndex) {
+    setFormValues(prev =>
+      prev.map((oldFormValue, oldValueIndex) =>
+        newValueIndex === oldValueIndex ? { label: oldFormValue.label, value: newValue.value } : oldFormValue,
+      ),
+    );
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {elements.map((inputElementData, index) => (
+        <InputElement
+          key={inputElementData.label}
+          value={formValues[index].value}
+          onChange={newValue => handleChange(newValue, index)}
+          {...inputElementData}
+        />
+      ))}
+      <CustomButton text="Submit" type="submit" fullMobile>
+        Submit
+      </CustomButton>
+    </form>
+  );
+}
+
+const InputElement = props => {
+  const { label, type, value, onChange, required } = props;
+  const propsToPass = { name: label, label, type, value, onChange, required, fullWidth: true };
+
+  switch (type) {
+    case 'text':
+      return <CustomTextField {...propsToPass} />;
+    case 'textarea':
+      return <CustomTextField {...propsToPass} />;
+    case 'number':
+      return <CustomTextField {...propsToPass} />;
+    case 'checkbox':
+      return <CustomCheckBox checked={value} {...propsToPass} />;
+    default:
+      return null;
+  }
+};
